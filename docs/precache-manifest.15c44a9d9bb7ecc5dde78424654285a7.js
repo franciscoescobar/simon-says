97c14b67d1e2f1b2087928cce26f9c44e05c82f1
franciscoescobar <franciscoescobar@users.noreply.github.com>
2019-08-01 18:37:56 -0300

    Upload build

---
+++ docs/precache-manifest.15c44a9d9bb7ecc5dde78424654285a7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e13bade70304fa7a5d2c615cf8844d41",
+    "url": "/simon-says/index.html"
+  },
+  {
+    "revision": "f224834ee51a0d93fa4f",
+    "url": "/simon-says/static/css/main.db60398c.chunk.css"
+  },
+  {
+    "revision": "168ee59ca029d2d46042",
+    "url": "/simon-says/static/js/2.fb38913b.chunk.js"
+  },
+  {
+    "revision": "f224834ee51a0d93fa4f",
+    "url": "/simon-says/static/js/main.d564d003.chunk.js"
+  },
+  {
+    "revision": "2ff488b8d7e0b2971c4e",
+    "url": "/simon-says/static/js/runtime~main.4578f539.js"
+  },
+  {
+    "revision": "88bfc182334ae7a726434f08640ab93a",
+    "url": "/simon-says/static/media/sound1.88bfc182.mp3"
+  },
+  {
+    "revision": "9ec72629ad1e665e8d8c6a78c3128ca3",
+    "url": "/simon-says/static/media/sound2.9ec72629.mp3"
+  },
+  {
+    "revision": "b704c8cb65390adbd6ccbfff6c60c97e",
+    "url": "/simon-says/static/media/sound3.b704c8cb.mp3"
+  },
+  {
+    "revision": "d24027d2ef9efda00cd3ba35dbc3053a",
+    "url": "/simon-says/static/media/sound4.d24027d2.mp3"
+  }
+]);
